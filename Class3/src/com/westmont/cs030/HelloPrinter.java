adaac485b747d007f68e784bbd22c38dbe288969
John Panos <johncpanos@gmail.com>
2020-01-10 09:59:50 -0800

    Fixed NamePrinter to HelloPrinter

--- Class3/src/com/westmont/cs030/HelloPrinter.java
+++ Class3/src/com/westmont/cs030/HelloPrinter.java
@@ -2,13 +2,13 @@
  * @author John Panos (Westmont College)
  * @contact jpanos@westmont.edu
  * @class CS-030
- * @assignment NamePrinter
+ * @assignment HelloPrinter
  * Created on Fri Jan 10 2020 9:50:24 AM
  */
 
 package com.westmont.cs030;
 
-public class NamePrinter {
+public class HelloPrinter {
 
 	public static void main(String[] args) {
 		System.out.println("John Panos");
