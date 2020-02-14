206bfc9b3b7068c9e9d6888822f27aa0a7605c9a
John Panos <johncpanos@gmail.com>
2020-02-13 22:07:38 -0800

    Add epsiteme 5

--- Episteme_05/src/edu/westmont/cs030/Quadrilateral.java
+++ Episteme_05/src/edu/westmont/cs030/Quadrilateral.java
@@ -1,58 +1,110 @@
 package edu.westmont.cs030;
 
+import java.util.Scanner;
+
 /**
  * 
  * @author john
  * @contact jpanos@westmont.edu
- * Date: Feb 5, 2020
+ * Date: Feb 13, 2020
  *
  */
 public class Quadrilateral {
-	private enum ShapeType { 
-		SQUARE,
-		RECTANGLE,
-		RHOMBUS,
-		PARALLELOGRAM,
-		TRAPEZOID
+	static class Point {
+		private double x;
+		private double y;
+		
+		public Point(double x, double y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		public double distanceToPoint(Point point) {
+			return Math.sqrt(
+					Math.pow((point.getX() - this.getX()), 2) + 
+					Math.pow((point.getY() - this.getY()), 2)
+			);
+		}
+		
+		public double slopeBetweenPoint(Point point) {
+			return (point.getY() - this.getY()) / (point.getX() - this.getX());
+		}
+		
+		public double getX() {
+			return this.x;
+		}
+		
+		public double getY() {
+			return this.y;
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("X: %f Y: %f", x, y);
+		}
 	}
 	
 	private final double EPSILON = 1E-11;
 	
-	private double x1, y1, x2, y2, x3, y3, x4, y4;
+	private Point p1, p2, p3, p4;
 	
 	public Quadrilateral(
-			double x1,
-			double y1,
-			double x2,
-			double y2,
-			double x3,
-			double y3,
-			double x4,
-			double y4
+			double x1, double y1,
+			double x2, double y2,
+			double x3, double y3,
+			double x4, double y4
 			) {
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.x3 = x3;
-		this.y3 = y3;
-		this.x4 = x4;
-		this.y4 = y4;
+		this.p1 = new Point(x1, y1);
+		this.p2 = new Point(x2, y2);
+		this.p3 = new Point(x3, y3);
+		this.p4 = new Point(x4, y4);
 	}
 	
 	public boolean isSquare() {
-		return false;
+		// Diagonal lengths
+		double distanceP1ToP3 = p1.distanceToPoint(p3);
+		double distanceP2ToP4 = p2.distanceToPoint(p4);
+		
+		// Side lengths
+		double distanceP1ToP2 = p1.distanceToPoint(p2);
+		double distanceP2ToP3 = p2.distanceToPoint(p3);
+		double distanceP3ToP4 = p3.distanceToPoint(p4);
+		double distanceP1ToP4 = p1.distanceToPoint(p4);
+		
+//		System.out.print(this.toString());
+//		System.out.println("distanceP1ToP2: " + distanceP1ToP2);
+//		System.out.println("distanceP1ToP3: " + distanceP1ToP3);
+//		System.out.println("distanceP1ToP4: " + distanceP1ToP4);
+//		
+//		System.out.println("p1->p2: " + distanceP1ToP2);
+//		System.out.println("p2->p3: " + distanceP2ToP3);
+//		System.out.println("p3->p4: " + distanceP3ToP4);
+//		System.out.println("p4->p1: " + distanceP1ToP4);
+//		System.out.println();
+		
+		return this.isEqual(distanceP1ToP3, distanceP2ToP4)
+				&& this.isEqual(distanceP1ToP2, distanceP2ToP3)
+				&& this.isEqual(distanceP3ToP4, distanceP1ToP4);
 	}
 	
 	public boolean isRectangle() {
-		return false;
+		if (this.isSquare()) {
+			return false;
+		}
+		
+		double distanceP1ToP3 = p1.distanceToPoint(p3);
+		double distanceP2ToP4 = p2.distanceToPoint(p4);
+		
+		return this.isEqual(distanceP1ToP3, distanceP2ToP4);
 	}
 	
 	public boolean isRhombus() {
-		return false;
+		return !this.isRectangle() && !this.isSquare();
 	}
 	
 	public boolean isParallelogram() {
-		return false;
+		System.out.println(this.toString());
+		return this.isEqual(this.p1.slopeBetweenPoint(p2), this.p3.slopeBetweenPoint(p4));
 	}
 	
 	public boolean isTrapezoid() {
@@ -60,7 +112,6 @@ public class Quadrilateral {
 	}
 	
 	public static void main(String[] args) {
-//		Scanner scan = new Scanner();
 //		if (this.isRectangle()) {
 //			
 //		}
@@ -68,5 +119,28 @@ public class Quadrilateral {
 	
 	private boolean isEqual(double a, double b) {
 		return (Math.abs(a - b)) < EPSILON;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append("p1: ");
+		builder.append(p1.toString());
+		builder.append("\n");
+		
+		builder.append("p2: ");
+		builder.append(p2.toString());
+		builder.append("\n");
+		
+		builder.append("p3: ");
+		builder.append(p3.toString());
+		builder.append("\n");
+		
+		builder.append("p4: ");
+		builder.append(p4.toString());
+		builder.append("\n");
+		
+		return builder.toString();
 	}
 }
--- Episteme_05/src/edu/westmont/cs030/QuadrilateralTest.java
+++ Episteme_05/src/edu/westmont/cs030/QuadrilateralTest.java
@@ -6,13 +6,60 @@ import org.junit.jupiter.api.Test;
 
 public class QuadrilateralTest {
 	@Test
-	public void testRectangle() {
+	public void testSquareRight() {
 		Quadrilateral q = new Quadrilateral(
 				0.0,0.0,
 				0.0,1.0,
 				1.0,1.0,
 				1.0,0.0
 		);
-		assertEquals(true, q.isSquare());
+		boolean isSquare = q.isSquare();
+		assertEquals(true, isSquare);
+	}
+	
+//	@Test
+//	public void testSquareRight2() {
+//		Quadrilateral q = new Quadrilateral(
+//				2.0, 0.0,
+//				3.0, -1.0,
+//				2.0, -2.0,
+//				1.0, -1.0
+//		);
+//		assertEquals(true, q.isSquare());
+//	}
+//	
+//	@Test
+//	public void testSquareWrong() {
+//		Quadrilateral q = new Quadrilateral(
+//				0.0,0.0,
+//				0.0,1.0,
+//				1.0,1.0,
+//				1.0,1.0
+//		);
+//		boolean isSquare = q.isSquare();
+//		assertEquals(false, isSquare);
+//	}
+//	
+//	@Test
+//	public void testSquareWrong2() {
+//		Quadrilateral q = new Quadrilateral(
+//				2.0, 0.0,
+//				4.0, -1.0,
+//				2.0, -2.0,
+//				1.0, -1.0
+//		);
+//		assertEquals(false, q.isSquare());
+//	}
+	
+	@Test
+	public void testParrallelogram() {
+		Quadrilateral q = new Quadrilateral(
+				0.0, 0.0,
+				3.0, 4.0,
+				7.0, 4.0,
+				4.0, 0.0
+		);
+		
+		assertEquals(true, q.isParallelogram());
 	}
 }
